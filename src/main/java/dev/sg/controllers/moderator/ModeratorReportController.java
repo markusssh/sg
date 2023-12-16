3f337658e7b13088939f52f0ce4441f0de315c3c
Mark <90873852+nevilleomg@users.noreply.github.com>
2023-12-16 20:51:34 +0300

    changed get to post

--- src/main/java/dev/sg/controllers/moderator/ModeratorReportController.java
+++ src/main/java/dev/sg/controllers/moderator/ModeratorReportController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,7 +28,7 @@ public class ModeratorReportController {
 
     private final ModeratorReportService moderatorReportService;
 
-    @GetMapping
+    @PostMapping
     public ResponseEntity<?> getReportsSorted(@RequestBody SortingDTO sortingDTO) {
         try {
             Pair<List<ReportDTO>,Integer> reportsSortedAndPageLimit = moderatorReportService.getReportsSortedAndPageLimit(sortingDTO);
